a446ccff55e1c2e34457a67ada2b483a2d8e5186
Alex Nisnevich <alex.nisnevich@gmail.com>
2015-04-04 13:47:08 -0700

    dalli should be ready for production

--- splooshed.rb
+++ splooshed.rb
@@ -44,8 +44,14 @@ DUMMY_WORDS = [
   "chopped"
 ]
 
-options = { :namespace => "app_v1", :compress => true }
-$dc = Dalli::Client.new('localhost:11211', options)
+$dc = Dalli::Client.new((ENV["MEMCACHIER_SERVERS"] || "localhost:11211").split(","),
+                    {:username => ENV["MEMCACHIER_USERNAME"],
+                     :password => ENV["MEMCACHIER_PASSWORD"],
+                     :namespace => "app_v1",
+                     :failover => true,
+                     :socket_timeout => 1.5,
+                     :socket_failure_delay => 0.2
+                    })
 
 def load_water_data 
   data = YAML.load(open("water_data.yaml"))
