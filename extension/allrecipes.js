fcf1ab1ac8f4c20402195cdfcb82f57b87d6286d
jacobnisnevich <jacob.nisnevich@gmail.com>
2015-04-05 00:06:58 -0700

    support for serious eats and epicurious

---
+++ extension/allrecipes.js
@@ -0,0 +1,38 @@
+var splooshed = "http://splooshed.herokuapp.com";
+
+var total = 0;
+
+$('h3').before($('<h3 style="color: #88A2B9">Water Usage: <span id="totalWaterUsage">0 gallons</span></h3>'));
+
+$('[itemprop="ingredients"]').map(function (i, x) {
+  var recipeText = $(x).find('.ingredient-amount').text() + " " + $(x).find('.ingredient-name').text();
+  $.post(splooshed + "/recipe_line", recipeText, function (data) {
+    console.log(data);
+    if (data.success) {
+      var roundedGallons = Math.round(data.gallons * 100) / 100;
+
+      if (roundedGallons > 500) {
+        var num = 8;
+      } else {
+        var num = Math.ceil(roundedGallons / 500 * 8);
+      }
+
+      var img = $("<img class='waterIcon' src='" + splooshed + "/images/droplet" + num + ".png' title='Water usage: " + roundedGallons + " gallons'>");
+
+      total = Math.round((total + roundedGallons) * 100) / 100 ;
+      $('#totalWaterUsage').text(total + " gallons")
+    } else {
+      var img = $("<img class='waterIcon' src='" + splooshed + "/images/droplet0.png' title='" + data.error + "'>");
+    }
+    $(x).append(img);
+  }); 
+});
+
+function addStyleString(str) {
+    var node = document.createElement('style');
+    node.innerHTML = str;
+    document.body.appendChild(node);
+}
+
+addStyleString('.waterIcon { position: absolute; left: -10px }');
+addStyleString('#liIngredient { position: relative; }')
